78938cb88f780adb6187039d876ac9ea84252e4b
AJ <pinedalezephyr@gmail.com>
2019-03-01 06:03:18 -0700

    final

---
+++ app/src/main/java/com/example/aj_news_app/NewsAdapter.java
@@ -0,0 +1,40 @@
+package com.example.aj_news_app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aj on 2/25/19.
+ */
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    public NewsAdapter(Context context) {
+        super(context, -1, new ArrayList<News>());
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView
+                    = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        TextView author = (TextView) convertView.findViewById(R.id.author);
+        TextView date = (TextView) convertView.findViewById(R.id.date);
+        TextView section = (TextView) convertView.findViewById(R.id.section);
+
+        News currentNews = getItem(position);
+        title.setText(currentNews.getTitle());
+        author.setText(currentNews.getAuthor());
+        date.setText(currentNews.getDate());
+        section.setText(currentNews.getSection());
+
+        return convertView;
+    }
+}
